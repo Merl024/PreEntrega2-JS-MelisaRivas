6fb4dd0d670c8dacf46979b453383275cd7144a3
MelisaRivas <rivasmelisa2024@gmail.com>
2023-06-19 20:44:55 -0600

    Terminando el simuladora, se han agregado varias funciones nuevas, pero aun necesita una modificacion para poder agregar datos a los arrays

---
+++ main.js
@@ -0,0 +1,231 @@
+/* Se hizo un simulador de convertidor de masas, volumen y longitudes,, en donde el usuario puede escoger entre convertir de libras (lb) a gramos, litros (l) a mililitros (ml), ademas se agrego la funcion de poder registrar el peso mensualmente, calculando el imc*/
+/* Se hizo un simulador de convertidor de masas, volumen y longitudes,, en donde el usuario puede escoger entre convertir de libras (lb) a gramos, litros (l) a mililitros (ml) y más*/
+
+// Empezamos declarando funciones para el convertidor
+//Kg a Lb
+function kgAlb(){
+   let kg = parseFloat(prompt("Ingrese la cantidad de kilogramos (kg) que desea convertir a libras (lb): "))
+   let conversionAlb = kg * 2.20462
+   alert(`${kg}kg a libras son ${conversionAlb.toFixed(2)}lb`) 
+}
+// Lb a g
+function lbAg(){
+   let lb = parseFloat(prompt("Ingrese la cantidad de libras (l) que desea convertir a gramos (g): "))
+   let conversionAg = lb * 453.592
+   alert(`${lb}lb a gramos son ${conversionAg.toFixed(2)}g`) 
+}
+//Gal a l
+function galAl(){
+   let gal = parseFloat(prompt("Ingrese la cantidad de galones (gal) que desea convertir a litros (l): "))
+   let conversionAl = gal * 3.78541
+   alert(`${gal}gal a litros son ${conversionAl.toFixed(2)}L`) 
+}
+// L a ml
+function lAml(){
+   let l = parseFloat(prompt("Ingrese la cantidad de litros (l) que desea convertir a mililitro (ml): "))
+   let conversionAml = l * 1000
+   alert(`${l}L a mililitro son ${conversionAml.toFixed(2)}ml`) 
+}
+//pies a m 
+function ftAm(){
+   let ft = parseFloat(prompt("Ingrese la cantidad de pies (ft) que desea convertir a metros (m): "))
+   let conversionAm = ft / 3.281
+   alert(`${ft}ft a metros son ${conversionAm.toFixed(2)}m`) 
+}
+// pulgadas a cm
+function inchAcm(){
+   let inch = parseFloat(prompt("Ingrese la cantidad de pulgadas (in) que desea convertir a centimetros (cm): "))
+   let conversionAcm = inch * 2.54
+   alert(`${inch}in a metros son ${conversionAcm.toFixed(2)}cm`) 
+}
+
+//Menu para el convertidor de msas, volumen y longitudes
+function menuConvertidor(){
+   do{
+      let opcionesCon = parseInt(prompt(`Estamos para ayudarte, ${usuario}. ¿Qué desea convertir?
+         0 - Salir del menu.
+         1 - Convertir masas.
+         2 - Convertir volumen.
+         3 - Convertir longitudes.`))
+         switch(opcionesCon){
+            //menu de opciones para convertir masas
+            case 1:
+               let opcionesDeMasa = parseInt(prompt(`¿Qué desea convertir?
+                  0 - Regresar al menu principal
+                  1 - Convertir Kilogramos (kg) a libras (lb)
+                  2 - Convertir libras (lb) a gramos (g)`))
+                  switch(opcionesDeMasa){
+                     case 1:
+                        kgAlb()
+                     break
+                     case 2:
+                        lbAg()
+                     break     
+                     case 0:
+                        alert(`Regresando al menú principal...`)
+                        opcionesDeMasa = true
+                     break   
+                     default:
+                        alert("Oh! oh!, no tenemos esa opción :(, intenta de nuevo.")
+                     break
+                  }
+            continue
+            //menu de opciones para convertir volumen
+            case 2:
+               let opcionesDeVolumen = parseInt(prompt(`¿Qué desea convertir?
+                  0 - Regresar al menu principal
+                  1 - Convertir galones (gal) a litros (l)
+                  2 - Convertir litros (l) a mililitro (ml)`))
+                  switch(opcionesDeVolumen){
+                     case 1:
+                        galAl()
+                     break
+                     case 2:
+                        lAml()
+                     break     
+                     case 0:
+                        alert(`Regresando al menú principal...`)
+                        opcionesDeVolumen = true
+                     break   
+                     default:
+                        alert("Oh! oh!, no tenemos esa opción :(, intenta de nuevo.")
+                     break
+                  }
+            continue
+            //menu de opciones para convertir longitudes
+            case 3:
+               let opcionesDeLongitudes = parseInt(prompt(`¿Qué desea convertir?
+                  0 - Regresar al menu principal
+                  1 - Convertir pies (ft) a metros (m)
+                  2 - Convertir pulgadas (in) a centimretos (cm)`))
+                  switch(opcionesDeLongitudes){
+                     case 1:
+                        ftAm()
+                     break
+                     case 2:
+                        inchAcm()
+                     break     
+                     case 0:
+                        alert(`Regresando al menú principal...`)
+                        opcionesDeLongitudes = true
+                     break   
+                     default:
+                        alert("Oh! oh!, no tenemos esa opción :(, intenta de nuevo.")
+                     break
+                  }  
+            continue
+            case 0:
+               alert(`Gracias por utilizar nuestro convertidor, ${usuario}. ¡Vuelva pronto!`)
+               salirMenu = true
+            break   
+            default:
+               alert('Oh! oh!, no tenemos esa opción :(, intenta de nuevo.')
+            break
+         }
+      }while(!salirMenu)      
+}
+
+//Menu para el control de peso
+class Registro{
+   constructor(id, pesoI, alturaI, fechaI){
+      this.id = id,
+      this.peso = pesoI,
+      this.altura = alturaI,
+      this.fecha = fechaI
+   }
+   mostrarInfoRegistro(){
+      alert(`Tu peso es ${this.peso}kg con una altura de ${this.altura}m, ingresado en la fecha ${this.fecha.toLocaleDateString()}`)
+   }
+}
+const registro = []
+function agregarDato(){
+   alert("Recuerda poner su peso en kilogramos y su altura en metros. (Al momento de poner la fecha pon el mes antes del día)")
+   let peso = parseFloat(prompt("Ingrese un nuevo peso:"))
+   let altura = parseFloat(prompt("Ingrese su nueva altura:"))
+   let fecha = prompt("Ingrese la fecha de este peso. (Ejemplo: 06/19/2023)")
+   const nuevoDato = new Registro(registro.length+1, peso, altura, fecha)
+   registro.push(nuevoDato)
+}
+
+
+function verRegistro(array){
+   alert('Tu registro es:')
+   for( let registro of array){
+      alert(registro.id, registro.pesoI, registro.alturaI, registro.fechaI)
+   }
+}
+
+function imc(){
+   let pesoKg = parseFloat(prompt("Ingrese su peso en kilogramos: "))
+   let alturaM = parseFloat(prompt("Ingrese su altura en metros: "))
+   let calculoImc = pesoKg / (alturaM * alturaM)
+   alert(`Su masa corporal es ${calculoImc.toFixed(2)}`)
+   if(calculoImc < 18.5){
+      alert("Usted está bajo de peso")
+      return
+   } if(18.5 <= calculoImc <= 24.9 ){
+      alert("Usted tiene un peso normal")
+      return
+   }if(25 <= calculoImc <= 29.9){ 
+      alert("Usted tiene sobrepeso")
+      return
+   }
+   if(calculoImc < 30){
+      alert("Usted tiene obesidad")
+      return
+   }else{
+      alert("Regrese al menu principal")
+   }
+}
+
+function registroDePeso(){
+   do{
+      let opcionesRe = parseInt(prompt(`Estamos para ayudarte, ${usuario}. ¿Qué desea hacer?
+      0 - Salir del menu.
+      1 - Agregar dato al registro.
+      2 - Ver registro.
+      3 - Calcular IMC.`))
+      switch(opcionesRe){
+         case 1: 
+            agregarDato()
+         break
+         case 2: 
+            verRegistro(registro)
+         break
+         case 3:
+            imc()
+         continue
+         case 0:
+            alert(`Gracias por utilizar nuestro convertidor, ${usuario}. ¡Vuelva pronto!`)
+            salirMenu = true
+         continue   
+         default:
+            alert('Oh! oh!, no tenemos esa opción :(, intenta de nuevo.')
+         break
+      }
+   }while(!salirMenu)
+}
+//MENÚ PRINCIPAL
+let usuario = prompt("Bienvenido/a a nuestro convertidor de masa, volumen y longitudes. Ingrese su nombre: ")
+let salirMenu = false
+do{
+   let opciones = parseInt(prompt(`Estamos para ayudarte, ${usuario}. ¿Qué desea hacer?
+         0 - Salir del menu.
+         1 - Llevar un registro del peso
+         2 - Convertir masas, longitudes y volumenes`))
+         switch(opciones){
+            case 1: 
+               registroDePeso()
+            continue
+            case 2:
+               menuConvertidor()
+            continue
+            case 0:
+               alert(`Gracias por utilizar nuestro convertidor, ${usuario}. ¡Vuelva pronto!`)
+               salirMenu = true
+            continue   
+            default:
+               alert('Oh! oh!, no tenemos esa opción :(, intenta de nuevo.')
+            break
+         }
+}while(!salirMenu)
